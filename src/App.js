d9c6c615dfca2dcc08356b2490a5edd68cdce2dd
AnnaigHoinart <annaighoinart@AnnaigHoinartMBP.local>
2019-12-12 12:01:42 +0200

    resizing fixed

--- src/App.js
+++ src/App.js
@@ -7,9 +7,27 @@ import Fighting from './Action_Games/Fighting';
 import './Action_Games/Shooter'
 import Shooter from './Action_Games/Shooter';
 
-const colStyle = {
+const colRight = {
   paddingLeft: "0px",
   paddingRight: "0px"
+};
+
+const colLeft = {
+  paddingRight: "0px",
+  paddingLeft: "0px"
+};
+
+const row = {
+  width: "100%",
+  margin: "0"
+}
+
+const in_btn = {
+  fontSize: "100%"
+};
+
+const git_btn = {
+  fontSize: "100%"
 };
 
 class HeadingText extends React.Component {
@@ -32,12 +50,12 @@ class SocialButtons extends React.Component {
       <div>
         <div className="social_buttons">
           <a href="https://www.linkedin.com/in/atabala/" target="_blank" className="in_button">
-            <button type="button" className="btn btn-primary">
+            <button type="button" className="btn btn-primary btn-md" style={in_btn}>
               <span className="fab fa-linkedin-in" />
             </button>
           </a>
           <a href="https://github.com/annaighoinart" target="_blank" className="git_button">
-            <button type="button" className="btn btn-dark git_button">
+            <button type="button" className="btn btn-dark git_button btn-md" style={git_btn}>
               <span className="fab fa-github" />
             </button>
           </a>
@@ -58,13 +76,12 @@ function App() {
 
       <h2 className="action_games_title">Action Games</h2>
 
-      <br />
       
-      <div className="row">
-        <div className="col" style={colStyle}>
+      <div className="row" style={row}>
+        <div className="col" style={colLeft}>
           <Fighting />
         </div>
-        <div className="col" style={colStyle}>
+        <div className="col" style={colRight}>
           <Shooter />
         </div>
       </div>
